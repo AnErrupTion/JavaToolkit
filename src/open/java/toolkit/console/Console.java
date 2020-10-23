dcfa911f77d82a536f6afe2e43025b6b866c4bfc
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2020-10-23 21:39:41 +0200

    Update Console
    Update Console, add clear() and readLine() but also add an ANSI warning if the system is currently running Windows. Finally, some methods have been readjusted (like ansiWrite and ansiWriteLine).

--- src/open/java/toolkit/console/Console.java
+++ src/open/java/toolkit/console/Console.java
@@ -1,7 +1,17 @@
 package open.java.toolkit.console;
 
+import open.java.toolkit.System;
+import open.java.toolkit.console.ansi.Foreground;
+
+import java.io.*;
+
 public class Console
 {
+    public static boolean showAnsiWarning = true;
+
+    public static final PrintStream SOUT = java.lang.System.out;
+    public static final InputStream SIN = java.lang.System.in;
+
     public static void writeLine(String text, LogType type)
     {
         write(text + "\n", type);
@@ -9,16 +19,33 @@ public class Console
 
     public static void write(String text, LogType type)
     {
-        System.out.printf("[%s] %s", type.toString(), text);
+        SOUT.printf("[%s] %s", type.toString(), text);
     }
 
-    public static void colorWriteLine(String ansiColorStart, String text, LogType type)
+    public static void ansiWriteLine(String ansiStart, String text, LogType type)
     {
-        colorWrite(ansiColorStart,text + "\n", type);
+        ansiWrite(ansiStart,text + "\r\n", type);
     }
 
-    public static void colorWrite(String ansiColorStart, String text, LogType type)
+    public static void ansiWrite(String ansiStart, String text, LogType type)
     {
-        System.out.printf(ansiColorStart + "[%s] %s", type.toString(), text);
+        if (System.isWindows() && showAnsiWarning)
+            writeLine("ANSI codes are not officially implemented in the Windows console, thus the colors and other codes may not work as expected.", LogType.WARNING);
+
+        SOUT.printf(ansiStart + "[%s] %s", type.toString(), text);
+    }
+
+    public static void clear() throws IOException, InterruptedException
+    {
+        if (System.isWindows())
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        else
+            SOUT.print(Foreground.CLEAR);
+    }
+
+    public static String readLine() throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(SIN));
+        return reader.readLine();
     }
 }
