8189768584162b7ef53373a0cdc91df7573de589
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-01-13 21:13:31 +0100

    Tons of changes!
    - Bunch of bug fixes to Console.

--- src/open/java/toolkit/console/Console.java
+++ src/open/java/toolkit/console/Console.java
@@ -9,22 +9,19 @@ public class Console
 {
     public static boolean showAnsiWarning = true;
 
-    public static final PrintStream SOUT = java.lang.System.out;
-    public static final InputStream SIN = java.lang.System.in;
-
     public static void writeLine(String text, LogType type)
     {
-        write(text + "\n", type);
+        write(text + System.newLine, type);
     }
 
     public static void write(String text, LogType type)
     {
-        SOUT.printf("[%s] %s", type.toString(), text);
+        java.lang.System.out.printf("[%s] %s", type.toString(), text);
     }
 
     public static void ansiWriteLine(String ansiStart, String text, LogType type)
     {
-        ansiWrite(ansiStart,text + "\r\n", type);
+        ansiWrite(ansiStart,text + System.newLine, type);
     }
 
     public static void ansiWrite(String ansiStart, String text, LogType type)
@@ -32,7 +29,7 @@ public class Console
         if (System.isWindows() && showAnsiWarning)
             writeLine("ANSI codes are not officially implemented in the Windows console, thus the colors and other codes may not work as expected.", LogType.WARNING);
 
-        SOUT.printf(ansiStart + "[%s] %s %s", type.toString(), text, Foreground.RESET);
+        java.lang.System.out.printf(ansiStart + "[%s] %s %s", type.toString(), text, Foreground.RESET);
     }
 
     public static void clear() throws IOException, InterruptedException
@@ -40,19 +37,22 @@ public class Console
         if (System.isWindows())
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         else
-            SOUT.print(Foreground.CLEAR);
+        {
+            java.lang.System.out.print(Foreground.CLEAR);
+            java.lang.System.out.flush();
+        }
     }
 
     public static void setTitle(String text) throws IOException, InterruptedException
     {
         if (!System.isWindows())
-            SOUT.println("\033]2;" + text + "\007");
+            java.lang.System.out.println("\033]2;" + text + "\007");
         else
             new ProcessBuilder("cmd", "/c", "title", text).inheritIO().start().waitFor();
     }
 
     public static String readLine() throws IOException
     {
-        return new BufferedReader(new InputStreamReader(SIN)).readLine();
+        return new BufferedReader(new InputStreamReader(java.lang.System.in)).readLine();
     }
 }
