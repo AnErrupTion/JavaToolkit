808621064a7ae72ceb5a7bf31810adb8c1ae25a4
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-06 22:45:27 +0100

    Add Files.writeBytes()

--- src/open/java/toolkit/files/Files.java
+++ src/open/java/toolkit/files/Files.java
@@ -15,7 +15,7 @@ public class Files
             String line;
 
             while ((line = br.readLine()) != null)
-                content.append(line + "\n");
+                content.append(line).append("\n");
 
             return content.toString();
         }
@@ -29,8 +29,26 @@ public class Files
         }
     }
 
+    public static byte[] readBytes(String path) throws IOException
+    {
+        File file = new File(path);
+        FileInputStream input = new FileInputStream(file);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        byte[] b = new byte[(int) file.length()];
+        int c;
+
+        while ((c = input.read(b)) != -1)
+            output.write(b, 0, c);
+
+        return output.toByteArray();
+    }
+
     public static void writeString(String path, String content, boolean append) throws IOException
     {
+        if (!fileExists(path))
+            createFile(path);
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, append)))
         {
             bw.write(content);
@@ -39,6 +57,9 @@ public class Files
 
     public static void writeLines(String path, String[] content, boolean append) throws IOException
     {
+        if (!fileExists(path))
+            createFile(path);
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, append)))
         {
             for (String str : content)
