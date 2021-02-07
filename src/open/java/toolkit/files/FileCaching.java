f161e34eb4569abc23f0ed964ec1b924c53baa58
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-07 09:10:37 +0100

    Try-catch instead of throw exception

--- src/open/java/toolkit/files/FileCaching.java
+++ src/open/java/toolkit/files/FileCaching.java
@@ -1,14 +1,13 @@
 package open.java.toolkit.files;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class FileCaching
 {
     private static final HashMap<String, byte[]> cachedFiles = new HashMap<>();
 
-    public static void cache(String file) throws IOException
+    public static void cache(String file)
     {
         String name = new File(file).getName();
         byte[] content = Files.readBytes(file);
