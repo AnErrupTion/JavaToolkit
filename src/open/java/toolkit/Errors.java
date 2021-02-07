be2c8ddd2f8601c9fc56ee6e65b982fa282d3935
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-07 10:41:13 +0100

    Add possibility to log errors

--- src/open/java/toolkit/Errors.java
+++ src/open/java/toolkit/Errors.java
@@ -1,12 +1,17 @@
 package open.java.toolkit;
 
 import open.java.toolkit.console.ansi.Foreground;
+import open.java.toolkit.files.Files;
 
 public class Errors
 {
     // Should be invoked ONLY when an exception occurs in JavaToolkit.
     public static void newError(String message)
     {
+        System.errorOccured = true;
         java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + message);
+
+        if (System.logErrors)
+            Files.writeString("javatoolkit_v" + System.toolkitVersion + "_" + System.dateAndTime.replace(" ", "_") + ".log", message, true);
     }
 }
