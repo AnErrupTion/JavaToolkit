d58ef872788a7428e9d0c8ac56a45d25c7e8bf4d
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-08 12:57:49 +0000

    Support showErrors

--- src/open/java/toolkit/Errors.java
+++ src/open/java/toolkit/Errors.java
@@ -10,7 +10,9 @@ public class Errors
     public static void newError(Exception ex)
     {
         System.errorOccured = true;
-        java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + ex.getMessage());
+        
+        if (System.showErrors)
+            java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + ex.getMessage());
         
         if (System.logErrors)
             Files.writeString("javatoolkit_v" + System.toolkitVersion + "_" + System.dateAndTime + ".log", ex.getMessage() + "\n" + Arrays.toString(ex.getStackTrace()), true);
