a6e51162300b19286e3b81445ab48a58ad74166f
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-08 12:04:19 +0000

    Add getArray()

--- src/open/java/toolkit/files/FileCaching.java
+++ src/open/java/toolkit/files/FileCaching.java
@@ -1,17 +1,17 @@
 package open.java.toolkit.files;
 
+import open.java.toolkit.System;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class FileCaching
 {
     private static final HashMap<String, byte[]> cachedFiles = new HashMap<>();
-
     public static void cache(String file)
     {
         String name = new File(file).getName();
         byte[] content = Files.readBytes(file);
-
         if (cachedFiles.containsKey(file))
         {
             uncache(file);
@@ -28,5 +28,10 @@ public class FileCaching
     public static byte[] get(String file)
     {
         return cachedFiles.get(new File(file).getName());
+    }
+
+    public static String[] getArray(String file)
+    {
+        return new String(get(file), StandardCharsets.UTF_8).split(System.newLine);
     }
 }
