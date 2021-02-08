5ac9823c791c3a1c5b27b1fcc63630c3c373b2ed
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-08 12:07:27 +0000

    Use Exceptions instead of Strings

--- src/open/java/toolkit/Errors.java
+++ src/open/java/toolkit/Errors.java
@@ -1,14 +1,18 @@
 package open.java.toolkit;
+
 import open.java.toolkit.console.ansi.Foreground;
 import open.java.toolkit.files.Files;
+import java.util.Arrays;
+
 public class Errors
 {
     // Should be invoked ONLY when an exception occurs in JavaToolkit.
-    public static void newError(String message)
+    public static void newError(Exception ex)
     {
         System.errorOccured = true;
-        java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + message);
+        java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + ex.getMessage());
+        
         if (System.logErrors)
-            Files.writeString("javatoolkit_v" + System.toolkitVersion + "_" + System.dateAndTime + ".log", message, true);
+            Files.writeString("javatoolkit_v" + System.toolkitVersion + "_" + System.dateAndTime + ".log", ex.getMessage() + "\n" + Arrays.toString(ex.getStackTrace()), true);
     }
 }
