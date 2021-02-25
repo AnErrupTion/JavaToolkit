6a9ca91825a1585c37c4fef3bee11dc3753f8691
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-02-25 20:00:00 +0000

    Add Process class

---
+++ src/open/java/toolkit/diagnostics/Process.java
@@ -0,0 +1,38 @@
+package open.java.toolkit.diagnostics;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Process
+{
+    public static ProcessBuilder createProcess(String... cmd)
+    {
+        return new ProcessBuilder(cmd).inheritIO();
+    }
+
+    public static Stream<ProcessHandle> getProcesses()
+    {
+        return ProcessHandle.allProcesses();
+    }
+
+    public static ProcessHandle getProcess(String name)
+    {
+        Stream<ProcessHandle> filtered = getProcesses().filter(process ->
+        {
+            Optional<String> cmd = process.info().command();
+            return cmd.isPresent() && cmd.get().contains(name);
+        });
+
+        Optional<ProcessHandle> handle = filtered.findFirst();
+        return handle.orElse(null);
+    }
+
+    public static void stopProcess(String name, boolean kill)
+    {
+        ProcessHandle handle = getProcess(name);
+        if (kill)
+            handle.destroyForcibly();
+        else
+            handle.destroy();
+    }
+}
