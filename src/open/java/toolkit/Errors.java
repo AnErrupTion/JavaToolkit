4f5eb81a0c285778329f2d964cfb1a7edb362e02
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2021-04-20 20:18:54 +0000

    Fix color not being reset when an error occured.

--- src/open/java/toolkit/Errors.java
+++ src/open/java/toolkit/Errors.java
@@ -11,10 +11,10 @@ public class Errors
     public static void newError(Exception ex)
     {
         System.errorOccurred = true;
-
+        
         if (System.showErrors)
-            java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + ex.getMessage());
-
+            java.lang.System.err.println(Foreground.RED_BOLD_BRIGHT + "[JAVATOOLKIT EXCEPTION] " + ex.getMessage() + Foreground.RESET);
+        
         if (System.logErrors)
             Files.writeString("javatoolkit_v" + System.toolkitVersion + "_" + System.dateAndTime + ".log", ex.getMessage() + "\n" + Arrays.toString(ex.getStackTrace()), true);
     }
